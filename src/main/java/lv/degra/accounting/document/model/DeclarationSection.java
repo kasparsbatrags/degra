2a36d3212e1ae7803dca82209ddff6f661a7d71f
Kaspars Batrags <kaspars.batrags@gmail.com>
2023-11-09 16:41:44 +0200

    - add STS document info

--- src/main/java/lv/degra/accounting/document/model/DeclarationSection.java
+++ src/main/java/lv/degra/accounting/document/model/DeclarationSection.java
@@ -4,14 +4,10 @@ import java.io.Serializable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,14 +15,14 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "document_transaction_type")
-public class DocumentTransactionType implements Serializable {
+@Table(name = "declaration_section")
+public class DeclarationSection implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", nullable = false)
 	private Integer id;
 
-	@Size(max = 2)
+	@Size(max = 10)
 	@Column(name = "code", nullable = false)
 	private String code;
 
@@ -34,10 +30,4 @@ public class DocumentTransactionType implements Serializable {
 	@Column(name = "name", nullable = false)
 	private String name;
 
-	@NotNull
-	@ManyToOne(fetch = FetchType.EAGER, optional = false)
-	@JoinColumn(name = "declaration_section_id", nullable = false)
-	private DeclarationSection declarationSection;
-
 }
-
