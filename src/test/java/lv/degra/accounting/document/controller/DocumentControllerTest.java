bca8780021df3544dde88368f573de8d41c989b3
Kaspars Batrags <kaspars.batrags@gmail.com>
2023-11-10 06:02:05 +0200

    - add STS document info to document form - add double input test

--- src/test/java/lv/degra/accounting/document/controller/DocumentControllerTest.java
+++ src/test/java/lv/degra/accounting/document/controller/DocumentControllerTest.java
@@ -1,21 +1,58 @@
 package lv.degra.accounting.document.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static lv.degra.accounting.configuration.DegraConfig.SUM_FORMAT_REGEX;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
 
-import lv.degra.accounting.system.exception.IncorrectSumException;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.stage.Stage;
 
-class DocumentControllerTest {
+class DocumentControllerTest extends ApplicationTest {
+
+	private static final String CORRECT_SUM_INPUT = "123.45";
+	private static final String INCORRECT_CORRECT_SUM_INPUT = "abc";
+	private DocumentController documentController;
+
+	@Override
+	public void start(Stage stage) {
+		// Initialize JavaFX environment for testing
+		new JFXPanel();
+		documentController = new DocumentController();
+	}
 
 	@Test
-	void testGetDouble() {
-		DocumentController documentController = new DocumentController();
+	void setFieldFormat_shouldApplyTextFormatter() {
 
-		String validDouble = "123.45";
-		assertEquals(123.45, documentController.getDouble(validDouble));
+		TextField textField = new TextField();
+		documentController.setFieldFormat(textField, SUM_FORMAT_REGEX);
 
-		String invalidDouble = "abc";
-		assertThrows(IncorrectSumException.class, () -> documentController.getDouble(invalidDouble));
+		TextFormatter<?> textFormatter = textField.getTextFormatter();
+		assertNotNull(textFormatter);
+
+		String validInput = CORRECT_SUM_INPUT;
+		interact(() -> {
+			textField.setText(validInput);
+		});
+		assertEquals(validInput, textField.getText(), "Valid input should be accepted");
+		textField.setText("");
+		String invalidInput = INCORRECT_CORRECT_SUM_INPUT;
+		interact(() -> {
+			textField.setText(invalidInput);
+		});
+		assertEquals("", textField.getText(), "Invalid input should be rejected");
+
+		// Test regex pattern
+		Pattern pattern = Pattern.compile(SUM_FORMAT_REGEX);
+		assertTrue(pattern.matcher(CORRECT_SUM_INPUT).matches(), "Valid input should match the pattern");
+		assertFalse(pattern.matcher(INCORRECT_CORRECT_SUM_INPUT).matches(), "Invalid input should not match the pattern");
 	}
 }
