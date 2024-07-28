d5f90ae5e10c67a4981876455f9c0309ee0d4ef4
Kaspars Batrags <kaspars.batrags@gmail.com>
2024-07-28 20:07:40 +0300

    - add dynamic rulles validation

---
+++ src/main/java/lv/degra/accounting/validation/model/ValidationRuleObject.java
@@ -0,0 +1,23 @@
+package lv.degra.accounting.validation.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class ValidationRuleObject {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Integer id;
+
+	private String name;
+	private String label;
+
+}
