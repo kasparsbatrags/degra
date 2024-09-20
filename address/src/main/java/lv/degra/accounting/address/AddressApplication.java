c11d45e4568db6a4672b35c502aa30266dff313a
Kaspars Batrags <kaspars.batrags@gmail.com>
2024-09-20 19:13:38 +0300

    + address strādā

--- address/src/main/java/lv/degra/accounting/address/AddressApplication.java
+++ address/src/main/java/lv/degra/accounting/address/AddressApplication.java
@@ -1,6 +1,9 @@
 package lv.degra.accounting.address;
 
 
+import lv.degra.accounting.core.address.register.exception.DownloadAddressDataException;
+import lv.degra.accounting.core.address.register.service.AddressRegisterService;
+import lv.degra.accounting.core.system.exception.ExtractZipFileException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,6 +11,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"lv.degra.accounting.core", "lv.degra.accounting.address"})
@@ -17,17 +21,23 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 public class AddressApplication {
 
+    private final AddressRegisterService addressRegisterService;
+
+    public AddressApplication(AddressRegisterService addressRegisterService) {
+        this.addressRegisterService = addressRegisterService;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(AddressApplication.class, args);
     }
 
 
-//    @Scheduled(cron = "${application.address-download-cron}")
-//    private void scheduleTaskUsingCronExpression() {
-//        try {
-//            downloadArData();
-//        } catch (ExtractZipFileException e) {
-//            throw new DownloadAddressDataException(e.getMessage() + e.getCause());
-//        }
-//    }
+    @Scheduled(cron = "${application.address-download-cron}")
+    private void scheduleTaskUsingCronExpression() {
+        try {
+            addressRegisterService.downloadArData();
+        } catch (ExtractZipFileException e) {
+            throw new DownloadAddressDataException(e.getMessage() + e.getCause());
+        }
+    }
 }
