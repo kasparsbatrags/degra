aae128dc1abe152c289d9a520f476a0dbd58eab5
Kaspars Batrags <kaspars.batrags@gmail.com>
2025-07-11 06:37:34 +0300

    - cīņa

--- native/freight/hooks/useTruckRouteForm.ts
+++ native/freight/hooks/useTruckRouteForm.ts
@@ -360,8 +360,6 @@ export function useTruckRouteForm(params: any) {
             const dateFrom = form.dateFrom instanceof Date ? form.dateFrom : new Date(form.dateFrom);
             const dateTo = form.dateTo instanceof Date ? form.dateTo : new Date(form.dateTo);
 
-			console.log("uuuuuuuuuuuuuuu: ",user)
-
             const truckRoutePage: TruckRoutePageDto = existingRoutePage || {
                 uid: '',
                 dateFrom: format(dateFrom, 'yyyy-MM-dd'),
